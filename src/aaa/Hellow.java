e129f68338a3f0cd681279ba1d8b0aff56a4a8cd
Pratap Chand <Pratap Chand@CTTLT013>
2019-11-30 11:21:51 +0530

    Ticket_302 Some amenmends done

---
+++ src/aaa/Hellow.java
@@ -0,0 +1,18 @@
+package aaa;
+
+public class Hellow {
+	
+void display()
+{
+	System.out.println("welcome");
+}
+public static void main(String[] args) {
+
+	Hellow h=new Hellow();
+	h.display();
+System.out.println();
+
+	
+}
+}
+
